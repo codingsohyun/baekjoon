fc49a908049a52a7cebce2709d6eab956b311ab1
codingsohyun <sshyuneee@gmail.com>
2023-07-06 16:49:31 +0900

    7/6 공부

--- 이분탐색/12015_가장긴증가하는부분수열2.py
+++ 이분탐색/12015_가장긴증가하는부분수열2.py
@@ -3,5 +3,21 @@ input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
+list = [0]
 
 for i in A :
+    if list[-1] < i:
+        list.append(i)
+    else:
+        left = 0
+        right = len(list)
+
+        while left < right:
+            mid = (left + right) // 2
+            if list[mid] < i:
+                left = mid + 1
+            else:
+                right = mid
+        list[right] = i
+
+print(len(list)-1)
